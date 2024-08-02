e4efc7d5b88f4f355bbd4abe18c2c17cb96b3e13
Rodrigo Mato <85580712+RodrigoMato00@users.noreply.github.com>
2024-08-02 00:25:03 -0300

    Manage error

--- ls/list.c
+++ ls/list.c
@@ -10,6 +10,11 @@ void list_directory(const char *path)
 	struct dirent *entry;
 
 	dir = opendir(path);
+	if (dir == NULL)
+	{
+		fprintf(stderr, "%s: cannot access %s: %s\n", "hls", path, strerror(errno));
+		return;
+	}
 	while ((entry = readdir(dir)) != NULL)
 	{
 		if (entry->d_name[0] != '.')
