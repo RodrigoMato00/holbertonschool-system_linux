d0853ee453af6461961efd1629f5f844c9772482
Rodrigo Mato <85580712+RodrigoMato00@users.noreply.github.com>
2024-08-02 10:19:12 -0300

    fix 0

--- ls/main.c
+++ ls/main.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 	char **dirs = malloc(argc * sizeof(char *));
 	char **files = malloc(argc * sizeof(char *));
 	int dir_count = 0, file_count = 0;
+	int count;
 
 	if (argc == 1)
 	{
@@ -23,7 +24,21 @@ int main(int argc, char *argv[])
 		process_args(argc, argv, dirs, files, &dir_count, &file_count);
 		selection_sort(dirs, dir_count);
 		selection_sort(files, file_count);
-		print_dirs_and_files(dirs, files, dir_count, file_count);
+		if (argc > 2)
+		{
+			print_dirs_and_files(dirs, files, dir_count, file_count);
+		}
+		else
+		{
+			for (count = 0; count < dir_count; count++)
+			{
+				list_directory(dirs[count]);
+			}
+			for (count = 0; count < file_count; count++)
+			{
+				list_directory(files[count]);
+			}
+		}
 		free(dirs);
 		free(files);
 	}
