8dabf1e3946e46b284cfa032a9fa5a62dc631e4b
Rodrigo Mato <85580712+RodrigoMato00@users.noreply.github.com>
2024-08-09 18:00:04 -0300

    refactor .h

--- ls/ls.h
+++ ls/ls.h
@@ -17,8 +17,10 @@ int is_directory(const char *path);
 int _strcmp(const char *str1, const char *str2);
 char _tolower(char c);
 void selection_sort(char **arr, int n);
-void process_args(int argc, char *argv[], char **dirs, char **files, int *dir_count, int *file_count);
-void print_dirs_and_files(char **dirs, char **files, int dir_count, int file_count);
+void process_args(int argc, char *argv[], char **dirs,
+			char **files, int *dir_count, int *file_count);
+void print_dirs_and_files(char **dirs, char **files, int
+				dir_count, int file_count);
 void selection_sort_dirent(struct dirent **arr, int n);
 int compare_dirent(const struct dirent *a, const struct dirent *b);
 struct dirent **count_entries(const char *path, int *num_entries);
