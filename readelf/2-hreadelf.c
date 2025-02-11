81c42bd000afbb68eda7b847ff32897afea85505
Rodrigo Mato <85580712+RodrigoMato00@users.noreply.github.com>
2025-02-11 20:28:22 -0300

    2 hreadelf

---
+++ readelf/2-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - read ELF file with exeve
+ * @argc: The number of command line arguments
+ * @argv: An array containing the program command line arguments
+ * @env:  An array containing the program environment
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *cmnd[] = {"/usr/bin/readelf", "-W", "-l", "", NULL};
+
+	(void)argc;
+	cmnd[3] = argv[1];
+	if (execve("/usr/bin/readelf", cmnd, env) == -1)
+	{
+		perror("execv");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
