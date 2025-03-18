37616ec854e28111e93071458d2c9d4a6290043a
rodrigomato00 <rodrigomato.rm@gmail.com>
2025-03-18 00:50:20 +0000

    add

--- nm_objdump/elf_parser.c
+++ nm_objdump/elf_parser.c
@@ -1,16 +1,72 @@
 #include "elf_parser.h"
 
 /**
+ * print_symbol_type - Determine symbol type character
+ * @sym: ELF symbol
+ * Return: Symbol type character
+ */
+char print_symbol_type(Elf64_Sym sym)
+{
+	char type;
+
+	switch (ELF64_ST_BIND(sym.st_info))
+	{
+	case STB_WEAK:
+		if (sym.st_shndx == SHN_UNDEF)
+			type = 'w';
+		else
+			type = 'W';
+		break;
+	default:
+		switch (sym.st_shndx)
+		{
+		case SHN_UNDEF:
+			type = 'U';
+			break;
+		case SHN_ABS:
+			type = 'A';
+			break;
+		case SHN_COMMON:
+			type = 'C';
+			break;
+		default:
+			switch (ELF64_ST_TYPE(sym.st_info))
+			{
+			case STT_OBJECT:
+				type = 'D';
+				break;
+			case STT_FUNC:
+				type = 'T';
+				break;
+			case STT_SECTION:
+				type = 'N';
+				break;
+			case STT_FILE:
+				type = 'F';
+				break;
+			default:
+				type = '?';
+			}
+		}
+	}
+	return (type);
+}
+
+/**
  * print_symbols - print ELF symbols
  * @file: FILE pointer
- * @shdr: Section headers
- * @strtab: String table
  * @symtab: Symbol table
+ * @strtab: String table
  */
 void print_symbols(FILE *file, Elf64_Shdr symtab, Elf64_Shdr strtab)
 {
-	Elf64_Sym *symbols = malloc(symtab.sh_size);
-	char *strs = malloc(strtab.sh_size);
+	Elf64_Sym *symbols;
+	char *strs;
+	size_t symbol_count, j;
+	char type;
+
+	symbols = malloc(symtab.sh_size);
+	strs = malloc(strtab.sh_size);
 
 	fseek(file, symtab.sh_offset, SEEK_SET);
 	fread(symbols, symtab.sh_size, 1, file);
@@ -18,39 +74,17 @@ void print_symbols(FILE *file, Elf64_Shdr symtab, Elf64_Shdr strtab)
 	fseek(file, strtab.sh_offset, SEEK_SET);
 	fread(strs, strtab.sh_size, 1, file);
 
-	size_t symbol_count = symtab.sh_size / sizeof(Elf64_Sym);
+	symbol_count = symtab.sh_size / sizeof(Elf64_Sym);
 
-	for (size_t j = 0; j < symbol_count; j++)
+	for (j = 0; j < symbol_count; j++)
 	{
-		char type;
-
-		switch (ELF64_ST_BIND(symbols[j].st_info))
-		{
-		case STB_WEAK:
-			type = (symbols[j].st_shndx == SHN_UNDEF) ? 'w' : 'W';
-			break;
-		default:
-			switch (symbols[j].st_shndx)
-			{
-			case SHN_UNDEF: type = 'U'; break;
-			case SHN_ABS:   type = 'A'; break;
-			case SHN_COMMON:type = 'C'; break;
-			default:
-				switch (ELF64_ST_TYPE(symbols[j].st_info))
-				{
-				case STT_OBJECT: type = 'D'; break;
-				case STT_FUNC:   type = 'T'; break;
-				case STT_SECTION:type = 'N'; break;
-				case STT_FILE:   type = 'F'; break;
-				default:         type = '?';
-				}
-			}
-		}
-
 		if (symbols[j].st_name != 0)
+		{
+			type = print_symbol_type(symbols[j]);
 			printf("%016lx %c %s\n",
 				symbols[j].st_value, type,
 				strs + symbols[j].st_name);
+		}
 	}
 
 	free(symbols);
@@ -63,30 +97,33 @@ void print_symbols(FILE *file, Elf64_Shdr symtab, Elf64_Shdr strtab)
  */
 void parse_elf(const char *filename)
 {
-	FILE *file = fopen(filename, "rb");
+	FILE *file;
+	Elf64_Ehdr ehdr;
+	Elf64_Shdr *shdr;
+	int i;
+
+	file = fopen(filename, "rb");
 	if (!file)
 	{
 		perror("Error al abrir archivo");
 		exit(EXIT_FAILURE);
 	}
 
-	Elf64_Ehdr ehdr;
 	fread(&ehdr, sizeof(ehdr), 1, file);
 
-	Elf64_Shdr shdr[ehdr.e_shnum];
+	shdr = malloc(sizeof(Elf64_Shdr) * ehdr.e_shnum);
 	fseek(file, ehdr.e_shoff, SEEK_SET);
 	fread(shdr, sizeof(Elf64_Shdr), ehdr.e_shnum, file);
 
-	for (int i = 0; i < ehdr.e_shnum; i++)
+	for (i = 0; i < ehdr.e_shnum; i++)
 	{
-		if (shdr[i].sh_type == SHT_SYMTAB || shdr[i].sh_type == SHT_DYNSYM)
+		if (shdr[i].sh_type == SHT_SYMTAB ||
+			shdr[i].sh_type == SHT_DYNSYM)
 		{
-			Elf64_Shdr symtab = shdr[i];
-			Elf64_Shdr strtab = shdr[symtab.sh_link];
-
-			print_symbols(file, symtab, strtab);
+			print_symbols(file, shdr[i], shdr[shdr[i].sh_link]);
 		}
 	}
 
+	free(shdr);
 	fclose(file);
 }
